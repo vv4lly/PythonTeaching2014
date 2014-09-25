2492a48a4b738c4d5742f929c97ec8c08c212220
Mark Foley <mark.foley@dit.ie>
2014-09-25 12:55:41 +0100

    refactored code

--- hailstone.py
+++ hailstone.py
@@ -3,22 +3,27 @@ Generate a Hailstone sequence
 '''
 
 number_str = input("Enter a positive integer:")
-number = int(number_str) 
-count = 0 
- 
-print("Starting with number:",number) 
-print("Sequence is: ", end=' ') 
- 
-while number > 1:  # stop when the sequence reaches 1 
- 
-    if number%2:        # number is odd 
-        number = number*3 + 1 
-    else:            # number is even 
-        number = number/2 
-    print(number,",", end=' ')   # add number to sequence 
- 
-    count +=1        # add to the count 
- 
-else: 
-    print()    # blank line for nicer output 
-    print("Sequence is ",count," numbers long") 
+number = int(number_str)
+count = 0
+
+print("Starting with number:", number)
+print("Sequence is: ", end=' ')
+
+# stop when the sequence reaches 1
+while number > 1:
+
+    # number is odd
+    if number % 2:
+        number = (number * 3) + 1
+
+    # number is even
+    else:
+        number /= 2
+
+    print(number, end=' ')  # add number to sequence
+
+    count += 1  # add to the count
+
+else:
+    print()  # blank line for nicer output
+    print("Sequence is ", count, " numbers long")
